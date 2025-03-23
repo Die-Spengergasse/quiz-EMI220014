fa8209272085ad2c9658eafdf4a39bce2ffaf630
Hümeyra <EMI220014@spengergasse.at>
2025-03-23 16:35:23 +0100

    Aufgabe gelöst

---
+++ src/main/java/at/spengergasse/entities/Answer.java
@@ -0,0 +1,56 @@
+package at.spengergasse.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "a_answers")
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "a_id")
+    private int id;
+
+    @Column(name = "a_correct")
+    private boolean correct;
+
+    @Column(name = "a_text")
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_q_id", nullable = false)
+    private Question question;
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "id=" + id +
+                ", correct=" + correct +
+                ", text='" + text + '\'' +
+                '}';
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+}
---
+++ src/main/java/at/spengergasse/entities/Question.java
@@ -0,0 +1,49 @@
+package at.spengergasse.entities;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "q_questions")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "q_id")
+    private int id;
+
+    @Column(name = "q_text")
+    private String text;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    private List<Answer> answers = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                '}';
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<Answer> answers) {
+        this.answers = answers;
+    }
+}
